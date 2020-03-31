c7e7afc22f85e122da7373acfa43402b24ce3a3b
paradoxalyty <46906762+paradoxalyty@users.noreply.github.com>
2020-03-31 11:12:16 +0200

    Add files via upload

---
+++ precache-manifest.1fc06a567ed35ca4a7de5bcf804447c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee52f19a484de1208ac6b22075c83df",
+    "url": "/ReactWarriors/index.html"
+  },
+  {
+    "revision": "82e1e9a3227ec16ee6ea",
+    "url": "/ReactWarriors/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "82e1e9a3227ec16ee6ea",
+    "url": "/ReactWarriors/static/js/2.2cddd29c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactWarriors/static/js/2.2cddd29c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25f3da190947309bdc5",
+    "url": "/ReactWarriors/static/js/main.3d96cec4.chunk.js"
+  },
+  {
+    "revision": "1e69c6a9dc79aedec20d",
+    "url": "/ReactWarriors/static/js/runtime-main.e4edc1f2.js"
+  }
+]);
